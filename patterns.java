7d0ec99e0a3ea4d82ecf5a15d5e627a26db4961e
Md Mozakkir khan <162160901+mozakkirkhan01@users.noreply.github.com>
2025-12-07 18:57:51 +0000

    patterns

--- patterns.java
+++ patterns.java
@@ -1,6 +1,18 @@
 public class patterns{
     public static void main(String args[])
     {
-        System.out.println("hello khan");
+        //character pattern print
+        char ch='A';
+        int n=4;
+        int k = 1;
+        for(int i = 0;i<n;i++)
+        {
+            for(int j=0; j<=i;j++)
+            {
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println("");
+        }
     }
 }
