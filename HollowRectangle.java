bae0173c7618c4ac08c3afc795a1e8e9b0cbbbd0
Md Mozakkir khan <162160901+mozakkirkhan01@users.noreply.github.com>
2025-12-11 18:13:54 +0000

    hollow Rectangle, pyramid

---
+++ HollowRectangle.java
@@ -0,0 +1,58 @@
+public class HollowRectangle
+{
+
+    public static void InvertRect(int row, int col)
+    {
+        for(int i=1;i<=row;i++)
+        {
+           for(int j=1;j<=col-i;j++)
+           {
+            System.out.print(" ");
+           }
+           for(int j=1;j<=i;j++)
+           {
+            System.out.print("* ");
+           }
+           System.out.println("");
+        }
+    }
+
+    public static void HollowRect(int row, int col)
+    {
+        //outerLoop
+        for(int i = 1; i<=row; i++)
+        {
+            for(int j=1; j<=col; j++)
+            {
+                if(i==1 || i==row || j==1 || j==col)
+                {
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String args[])
+    {
+        int n= 4;
+        int x = 5;
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j=1;j<=x;j++)
+            {
+                if(i==1 || i==n || j==1 || j==x)
+                {
+                    System.out.print("("+i+","+j+")");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("");
+        }
+        // HollowRect(6, 9);
+        InvertRect(6,9);
+    }
+}
