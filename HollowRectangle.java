d2d419b03eaa3811668e60e2ac0789b84ab7099b
Md Mozakkir khan <162160901+mozakkirkhan01@users.noreply.github.com>
2025-12-12 16:21:17 +0000

    HollowRectangle

--- HollowRectangle.java
+++ HollowRectangle.java
@@ -1,5 +1,18 @@
 public class HollowRectangle
 {
+    public static void FloydTriangle(int n)
+    {
+        int m = 1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(m);
+                m++;
+            }
+            System.err.println("");
+        }
+    }
 
     public static void InvertRect(int row, int col)
     {
@@ -36,23 +49,8 @@ public class HollowRectangle
     }
     public static void main(String args[])
     {
-        int n= 4;
-        int x = 5;
-        for(int i = 1;i<=n;i++)
-        {
-            for(int j=1;j<=x;j++)
-            {
-                if(i==1 || i==n || j==1 || j==x)
-                {
-                    System.out.print("("+i+","+j+")");
-                }
-                else{
-                    System.out.print("  ");
-                }
-            }
-            System.out.println("");
-        }
         // HollowRect(6, 9);
-        InvertRect(6,9);
+        // InvertRect(6,9);
+        FloydTriangle(4);
     }
 }
